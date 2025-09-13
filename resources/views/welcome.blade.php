5539826a759647efe40b1ab80db8435a08e9fcd0
daniertg <febriandani00@gmail.com>
2025-09-14 00:42:07 +0700

    ad

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Laravel 12 + React</title>
+    <title>Laravel 12 + React asd</title>
     @viteReactRefresh
     @vite('resources/js/app.jsx')
 </head>
